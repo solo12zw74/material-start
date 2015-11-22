f77df2dc040cf65bc31329a9d02648a97f1873be
solo12zw74 <solo12zw74@gmail.com>
2015-11-22 23:26:55 +0500

    Add new icons Remove old icons Add new views Install ui-router

--- app/src/wishes/WishService.js
+++ app/src/wishes/WishService.js
@@ -16,7 +16,7 @@
     var wishes = [
       {
         name: 'Happy',
-        image: 'http://lorempixel.com/300/200/abstract/',
+        image: 'http://lorempixel.com/300/200/sports/',
         content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
       },
       {
@@ -26,12 +26,12 @@
       },
       {
         name: 'Beauty',
-        image: 'http://lorempixel.com/300/200/abstract/',
+        image: 'http://lorempixel.com/300/200/business/',
         content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
       },
       {
         name: 'Power',
-        image: 'http://lorempixel.com/300/200/abstract/',
+        image: 'http://lorempixel.com/300/200/cats/',
         content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
       },
       {
@@ -41,7 +41,7 @@
       },
       {
         name: 'Water',
-        image: 'http://lorempixel.com/300/200/abstract/',
+        image: 'http://lorempixel.com/300/200/transport/',
         content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
       }
     ];
